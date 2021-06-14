d3c6603a0222c0bbbb93200ebe5389ee798cd4fd
sassela <sassela@users.noreply.github.com>
2021-06-14 13:22:00 +0100

    Setup Chinook DB before running update_rows

--- docker-run-test/update_rows.sh
+++ docker-run-test/update_rows.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+setup_db() {
+  sqlcmd \
+    -S 127.0.0.1,21433 \
+    -U sa \
+    -P hasuraMSSQL1 \
+    -i Chinook_SqlServer.sql
+}
+
 update_rows() {
   sqlcmd \
-    -S localhost:1433 \
+    -S 127.0.0.1,21433 \
     -U sa \
-    -P testPassword123 \
-    -Q "UPDATE Album SET Title = CONCAT(NewID(), GETUTCDATE()) WHERE 1=1;"
+    -P hasuraMSSQL1 \
+    -Q "UPDATE [Chinook].[dbo].[Album] SET Title = CONCAT(NewID(), GETUTCDATE()) WHERE 1=1;"
 }
 sleep_interval_ms=1000
 test_duration_seconds=60
 float_sleep_interval=$(awk -v interval=$sleep_interval_ms 'BEGIN { print interval/1000 }')
 total_iters=$(( 1000/sleep_interval_ms * test_duration_seconds ))
+# setup_db
 echo "Starting at: $(date)"
 for i in $(seq $total_iters)
 do
