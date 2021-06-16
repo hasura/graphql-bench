0078698eb9e14fc5f715def3717dbe4878e67560
sassela <sassela@users.noreply.github.com>
2021-06-16 15:12:18 +0100

    Add psql & mssql_wait

--- containers/psql-setup-events-table.sh
+++ containers/psql-setup-events-table.sh
@@ -6,4 +6,13 @@ PGADDRESS=localhost:5430
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 EVENTS_TABLE=$SCRIPT_DIR/setup_events_table.sql
 
+function pg_wait {
+  echo -n "Waiting for postgres to come up"
+  until ( psql postgres://$PGUSER:$PGPASS@$PGADDRESS/postgres -c '\l' ) &>/dev/null; do
+    echo -n '.' && sleep 0.2
+  done
+  echo " Ok"
+}
+
+pg_wait
 psql postgres://$PGUSER:$PGPASS@$PGADDRESS/postgres <$EVENTS_TABLE
