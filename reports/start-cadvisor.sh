5d938fd51d4f3f092a09fe30cd6c529ed4a3948e
sassela <sassela@users.noreply.github.com>
2021-06-16 21:27:20 +0100

    Run cadvisor for docker stats

---
+++ reports/start-cadvisor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+VERSION=v0.36.0 # use the latest release version from https://github.com/google/cadvisor/releases
+
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8090:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:$VERSION
